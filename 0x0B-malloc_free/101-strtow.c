6ce84256316205f9a851dee0c3eb4233386b342d
shatrahkemi <shatrahkemigisha@gmail.com>
2023-08-07 02:15:10 -0700

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -42,7 +42,13 @@ char **strtow(char *str)
 	util(words, str);
 	words[len] = NULL;
 	return (words);
+}
 
+/**
+ * util - a util function for fetching words into an array
+ * @words: the strings array
+ * @str: the string
+ */
 void util(char **words, char *str)
 {
 	int i, j, start, flag;
@@ -70,22 +76,3 @@ void util(char **words, char *str)
 		create_word(words, str, start, i, j);
 }
 
-/**
- * create_word - creates a word and insert it into the array
- * @words: the array of strings
- * @str: the string
- * @start: the starting index of the word
- * @end: the stopping index of the word
- * @index: the index of the array to insert the word
- */
-void create_word(char **words, char *str, int start, int end, int index)
-{
-	int i, j;
-
-	i = end - start;
-	words[index] = (char *)malloc(sizeof(char) * (i + 1));
-
-	for (j = 0; start < end; start++, j++)
-		words[index][j] = str[start];
-	words[index][j] = '\0';
-}
